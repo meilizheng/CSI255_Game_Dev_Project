09362c8fdcc318d01580c55ebf5bad0e7c93e3c2
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-04-24 19:28:19 -0700

    update the code
    change and update

---
+++ Assets/mathProblem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mathProblem : MonoBehaviour
+{
+    public Text firstNumber;
+    public Text secondNumber;
+
+    
+    
+}
