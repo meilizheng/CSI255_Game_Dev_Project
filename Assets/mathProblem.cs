f477af26e77fb37751a09ce74f4835aae5ab3301
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-04-28 12:34:48 -0700

    UPDATE DODE

--- Assets/mathProblem.cs
+++ Assets/mathProblem.cs
@@ -8,6 +8,62 @@ public class mathProblem : MonoBehaviour
     public Text firstNumber;
     public Text secondNumber;
 
-    
+    public Text answer1;
+    public Text answer2;
+    public List<int> easyMathLish = new List<int>();
+
+    public int randomFirstNumber;
+    public int randomSecondNumber;
+
+    int firstNumberInProblem;
+    int secondNumberInProblem;
+    int answerOne;
+    int answerTwo;
+    int displayRandowAnswer;
+    int randomAnswerPlacement;
+    public int currentAnswer;
+    public Text rightOrWrong_Text;
+
+    private void Start()
+    {
+        DisplayMathProblem();
+    }
+
+    public void DisplayMathProblem()
+    {
+        //generate a random number as the first and second number
+        randomFirstNumber = Random.Range(0, easyMathLish.Count + 1);
+        randomSecondNumber = Random.Range(0,easyMathLish.Count + 1);
+        //assing the first and second number
+        firstNumberInProblem = randomFirstNumber;
+        secondNumberInProblem = randomSecondNumber;
+        //this is where you can either addition, subtraction, multiplication or division
+        answerOne = firstNumberInProblem + secondNumberInProblem;
+        displayRandowAnswer = Random.Range(0, 2);
+        if(displayRandowAnswer == 0)
+        {
+            answerTwo = answerOne + Random.Range(1, 5);
+        }
+        else
+        {
+            answerTwo = answerOne - Random.Range(1, 5);    
+        }
+
+        firstNumber.text ="" + firstNumberInProblem;
+        secondNumber.text ="" + secondNumberInProblem;
+        randomAnswerPlacement = Random.Range(0, 2);
+        if(randomAnswerPlacement == 0)
+        {
+            answer1.text = "" + answerOne;
+            answer2.text = "" + answerTwo;
+            currentAnswer = 0;
+        }
+        else
+        {
+            answer1.text = "" + answerTwo;
+            answer2.text = "" + answerOne;
+            currentAnswer = 1;
+        }
+    }
     
 }
